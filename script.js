b299ce7e609b2d49c46f1c84846b7c07d763bed5
Mariana Barbosa <barbosasilvamariana@gmail.com>
2024-08-26 21:59:32 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,64 @@
+const textArea = document.querySelector(".text-area");
+const mensagem = document.querySelector(".mensagem");
+
+
+
+function btnEncriptar() {
+const textoEncriptado = encriptar(textArea.value);
+mensagem.value = textoEncriptado;
+textArea.value = "";
+}
+
+function encriptar(stringEncriptada) {
+
+    let matrizCodigo = [["e", "enter"], ["i", "imes"], ["a", "ai"],["o","ober"] ,["u", "ufat"]];
+    stringEncriptada = stringEncriptada.toLowerCase();
+
+    for(let i = 0; i <matrizCodigo.length; i++) {
+        if (stringEncriptada.includes(matrizCodigo[i][0]))  {
+            stringEncriptada = stringEncriptada.replaceAll(matrizCodigo[i][0], matrizCodigo[i][1]);
+        }
+
+    }
+
+    return stringEncriptada
+}
+
+
+
+
+function btnDesencriptar() {
+    const textoDesencriptado = desencriptar(textArea.value);
+    mensagem.value = textoDesencriptado;
+    textArea.value = "";
+    }
+    
+    function desencriptar(stringDesencriptada) {
+    
+        let matrizCodigo = [["e", "enter"], ["i", "imes"], ["a", "ai"],["o","uber"] ,["u", "ufat"]];
+        stringDesencriptada = stringDesencriptada.toLowerCase();
+    
+        for(let i = 0; i <matrizCodigo.length; i++) {
+            if (stringDesencriptada.includes(matrizCodigo[i][1]))  {
+                stringDesencriptada = stringDesencriptada.replaceAll(matrizCodigo[i][1], matrizCodigo[i][0])
+            }
+    
+        }
+    
+        return stringDesencriptada
+    }
+    
+    
+
+    function copiarTexto() {
+        mensagem.select();
+        mensagem.setSelectionRange(0, 99999); 
+        navigator.clipboard.writeText(mensagem.value)
+            .then(() => {
+                mensagemCopiada.style.display = "block";
+                setTimeout(() => {
+                    mensagemCopiada.style.display = "none";
+                }, 40000); // A mensagem "Copiado!" desaparecerá após 40 segundos
+            })
+            
+    }
